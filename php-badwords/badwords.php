4c8df1d8fbcfb96a83366afee2fd33972ccc171a
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-02-06 15:54:05 +0100

    Finish

--- php-badwords/badwords.php
+++ php-badwords/badwords.php
@@ -1,9 +1,13 @@
 <?php
 //Prendo i valori dal form nell HTML
+$bad_word = $_GET['badword'];
+$text = $_GET['text'];
 
 
+$censured_text = str_replace($bad_word, '***' , $text);
 
-
+$original_length = strlen($text);
+$filtredtext_length = strlen($censured_text);
 
 ?>
 
@@ -17,7 +21,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bad Words</title>
+    <title>Censured Version</title>
 
     <!-- BOOTSRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -25,6 +29,19 @@
 
 </head>
 <body>
-    
+<div class="container my-5">
+        <!-- ORIGINAL TEXT -->
+        <div class="border p-5 mb-3">
+            <h3>IL TESTO ORIGINALE È:</h3>
+            <p class="fw-bold">"<?=$text ?>"</p>
+            <p>La lunghezza del testo originale è di <span class="text-danger fw-bold"><?= $original_length?> </span> caratteri.</p>
+        </div>
+          <!-- CENSURED TEXT -->
+        <div class="border p-5">
+            <h3>IL TESTO CENSURATO É:</h3>
+            <p class="fw-bold">"<?=$censured_text ?>"</p>
+            <p>La lunghezza del testo censurato è di <span class="text-success fw-bold"><?= $filtredtext_length?> </span> caratteri.</p>
+        </div>
+    </div>
 </body>
 </html>
